b1a18f375f0737e71094d6aa60b6fbe4779b2c17
Rômulo Henri <romulohenri000@gmail.com>
2024-01-10 11:07:49 -0300

    add: growing-plants

---
+++ growing-plants/Assets/Scripts/corn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class pop : MonoBehaviour
+{
+    // to animator
+    public Animator animator;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space key was pressed.");
+            // Restart the animator
+            animator.Play("corn-growing");
+        }
+    }
+}
